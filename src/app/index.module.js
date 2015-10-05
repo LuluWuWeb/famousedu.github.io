093f1f840580d8a0be9b8b9413d7275bb1e6fa62
WXLWEB <894147172@qq.com>
2015-10-05 17:39:56 +0800

    init angular gulp model

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('famouseduGithubIo', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'ui.bootstrap']);
+
+})();
