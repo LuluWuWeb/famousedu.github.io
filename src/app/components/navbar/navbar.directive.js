b33c6c181476bdcfa89cc135abbd88d5fc69db9f
WXLWEB <894147172@qq.com>
2015-11-09 22:04:41 +0800

    upload index

--- src/app/components/navbar/navbar.directive.js
+++ src/app/components/navbar/navbar.directive.js
@@ -26,6 +26,17 @@
 
       // "vm.creation" is avaible by directive option "bindToController: true"
       vm.relativeDate = moment(vm.creationDate).fromNow();
+      var t;
+      $(window).scroll(function () {
+        t = document.documentElement.scrollTop || document.body.scrollTop;
+        if (t > 0) {
+          $('.navbar').removeClass('navbar-default').addClass('navbar-default-blue');
+          $('.marker').css('background-color', '#025499');
+        } else {
+          $('.navbar').removeClass('navbar-default-blue').addClass('navbar-default');
+          $('.marker').css('background-color', '#fff');
+        }
+      });
     }
   }
 
